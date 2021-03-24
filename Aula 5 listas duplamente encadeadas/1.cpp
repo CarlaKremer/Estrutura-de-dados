e0d0b9833f0517b2d6a85b9885b9b0460f8fbd52
carlakro <carlakremerf@gmail.com>
2021-03-24 01:22:12 -0300

    inserir posição

--- Aula 5 listas duplamente encadeadas/1.cpp
+++ Aula 5 listas duplamente encadeadas/1.cpp
@@ -1,4 +1,4 @@
-   // Lista din‚mica duplamente encadeada
+// Lista dinùmica duplamente encadeada
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
@@ -9,7 +9,7 @@ int cont;
 
 struct no
 {
-	int valor; //conteudo
+	int valor;	  //conteudo
 	no *proximo;  //aponta para proximo
 	no *anterior; //aponta para anterior
 };
@@ -21,19 +21,20 @@ void inserirNoInicio(int valor)
 	novo = (no *)malloc(sizeof(no));
 	novo->valor = valor;
 	if (cabeca == NULL)
-	{                   //o anterior ao valor inserido aponta para null e o proximo tbm, e o novo valor se torna o cabeca/inicio 
-		novo-> anterior = NULL; 
+	{ //o anterior ao valor inserido aponta para null e o proximo tbm, e o novo valor se torna o cabeca/inicio
+		novo->anterior = NULL;
 		novo->proximo = NULL;
-		cabeca = novo;    
-		cont= cont+1;      
+		cabeca = novo;
+		//cont = cont + 1;
 	}
-	else 
+	else
 	{
 		cabeca->anterior = novo; //coloca valor no inicio
-		novo->anterior = NULL;   //valor novo aponta null para anterior
-		novo-> proximo = cabeca; //o valor inserido aponta para a antiga cabeca como proximo
-		cabeca = novo;           //novo valor È o cabeca
+		novo->anterior = NULL;	 //valor novo aponta null para anterior
+		novo->proximo = cabeca;	 //o valor inserido aponta para a antiga cabeca como proximo
+		cabeca = novo;			 //novo valor È o cabeca
 	}
+	cont = cont + 1;
 	printf("\nO valor [%d] inserido com successo!", novo->valor);
 	getch();
 	system("cls");
@@ -42,10 +43,10 @@ void inserirNoInicio(int valor)
 void mostraLista()
 {
 	no *aux;
-	aux = cabeca; //tem que usar auxiliar par an„o perder o inicio da lista
-	while(aux != NULL)
+	aux = cabeca; //tem que usar auxiliar para n„o perder o inicio da lista
+	while (aux != NULL)
 	{
-		printf(" %d,",aux->valor);
+		printf(" %d,", aux->valor);
 		aux = aux->proximo;
 	}
 	printf("\n\nLista exibida com successo!");
@@ -56,10 +57,10 @@ void mostraLista()
 void mostraListaInversa()
 {
 	no *aux = cabeca;
-	
-	if(aux != NULL)
+
+	if (aux != NULL)
 	{
-		while(aux->proximo != NULL)
+		while (aux->proximo != NULL)
 		{
 			aux = aux->proximo;
 		}
@@ -67,8 +68,7 @@ void mostraListaInversa()
 		{
 			printf(" %d,", aux->valor);
 			aux = aux->anterior;
-		}
-		while(aux != NULL); //chegou no primeiro elemento, n„o precisa exibir mais	
+		} while (aux != NULL); //chegou no primeiro elemento, n„o precisa exibir mais
 	}
 	else
 	{
@@ -83,83 +83,96 @@ void inserirNoFim(int valor)
 {
 	no *novo;
 	no *aux = cabeca;
-	
+
 	novo = (no *)malloc(sizeof(no));
 	novo->valor = valor;
-	
-	if(aux != NULL)
+
+	if (aux != NULL)
 	{
-		while(aux->proximo != NULL)
+		while (aux->proximo != NULL)
 		{
 			aux = aux->proximo;
 		}
-		aux->proximo = novo; //colocando valor no final
+		aux->proximo = novo;  //colocando valor no final
 		novo->proximo = NULL; //proximo ao novo valor È NULL
-		novo->anterior = aux ;  //e o anterior vai ser o auxiliar que era o ultimo antes de inserir o novo valor
+		novo->anterior = aux; //e o anterior vai ser o auxiliar que era o ultimo antes de inserir o novo valor
 	}
 	else //lista vazia:
 	{
 		cabeca = novo;
-		novo->proximo= NULL;
+		novo->proximo = NULL;
 		novo->anterior = NULL;
 	}
-	cont= cont+1;    
-	printf("\nO valor [%d] inserido com successo!", novo->valor);
-	getch();
-	system("cls");	
-}
-
-void inserirElementoPosicao(int valor)
-{
-	printf("\nDigite o elemento que ser· inserido na posiÁ„o desejada: ");
-	scanf("%d", &valor);
-	no *novo; *aux = cabeca;
-	novo = (no *)malloc(sizeof(no));
-	novo->valor = valor;
-	if (cabeca == NULL)
-	{                   //o anterior ao valor inserido aponta para null e o proximo tbm, e o novo valor se torna o cabeca/inicio 
-		novo-> anterior = NULL; 
-		novo->proximo = NULL;
-		cabeca = novo;          
-	}
-	else if(posicao == 1)
-	{
-		novo-> anterior = NULL; 
-		novo->proximo = NULL;
-		cabeca = novo; 
-	}
-	else if(posicao >= cont+1)
-	{
-		
-	}
-	{
-		cabeca->anterior = novo; //coloca valor no inicio
-		novo->anterior = NULL;   //valor novo aponta null para anterior
-		novo-> proximo = cabeca; //o valor inserido aponta para a antiga cabeca como proximo
-		cabeca = novo;           //novo valor È o cabeca
-	}
-	//else
-	//{
-		
-	//}
-	printf("\nO valor [%d] inserido com successo!", novo->valor);
+	cont = cont + 1;
+	printf("\nO valor [%d] foi inserido com successo!", novo->valor);
 	getch();
 	system("cls");
-	
-	
+}
+
+void inserirElementoPosicao(int posicao)
+{
+	no *novo, *aux = cabeca, *aux2;
+	int valor;
+
+	printf("\nDigite o elemento que ser· inserido: ");
+	scanf("%d", &valor);
+
+	novo = (no *)malloc(sizeof(no));
+	novo->valor = valor;
+
+	if (cabeca = NULL)
+	{
+		novo->anterior = NULL;
+		novo->proximo = NULL;
+		cabeca = novo;
+	}
+	else if (posicao == 1) //se for o primeiro elemento
+	{
+		cabeca->anterior = novo; //anterior a cabeca recebe novo
+		novo->anterior = NULL;	 //anterior a novo È NULL
+		novo->proximo = cabeca;	 //proximo a novo È
+	}
+	else if (posicao == cont + 1) //se o elemento for o ˙ltimo
+	{
+		while (aux->proximo != NULL) //enquanto n„o chegar no final
+		{
+			aux = aux->proximo; // vai passando pro proximo
+		}
+		aux->proximo = novo;  //aux proximo È novo
+		novo->proximo = NULL; //proximo È null pois final
+		novo->anterior = aux; //anterior È o È aux
+	}
+	else
+	{ //caso seja alguma posiÁ„o no meio
+		printf("\nPosiÁ„a que ser· inserida: %d", posicao);
+		for (int i = 1; i < posicao; i++)
+		{
+			aux = aux->proximo; //vai passando atÈ chegar na posiÁ„o
+		}
+
+		aux2 = aux->anterior;  //aux2 recebe o anterior de aux
+		novo->proximo = aux;   //proximo recebe aux
+		aux->anterior = novo;  //anterior aux recebe o novo
+		novo->anterior = aux2; //novo anterior recebe o aux2 que era o anterior do aux
+		aux2->proximo = novo;  //aux2 proximo recebe novo
+	}
+	cont++;
+	printf("\nElemento %d foi inserido com sucesso", novo->valor);
+	getch();
+	system("cls");
 }
 
 void tamanhoLista()
 {
 	no *aux = cabeca;
-	int contador=0;
+	int contador = 0;
 	//for (int i=0; aux != NULL; i++)
-	if (aux!= NULL)
+	if (aux != NULL)
 	{
-		while(aux != NULL)
+		while (aux != NULL)
 		{
-		aux = aux->proximo;
-		contador++;
+			aux = aux->proximo;
+			contador++;
 		}
 		printf("\nA lista possui %d elementos", contador);
 	}
@@ -169,67 +182,59 @@ void tamanhoLista()
 	}
 	getch();
 	system("cls");
-	
 }
-
 
 main()
 {
-	setlocale(LC_ALL,"Portuguese");
+	setlocale(LC_ALL, "Portuguese");
 	int op, valor, posicao;
-	
-	do 
+
+	do
 	{
 		printf("--Digite 0 para sair-- \n");
 		printf("\nDigite 1 para inserir no inÌcio ");
 		printf("\nDigite 2 para mostrar a lista");
 		printf("\nDigite 3 para exibir a lista ao contr·rio");
 		printf("\nDigite 4 para inserir no final da lista");
-		printf("\nDigite 5 para escolher em que posiÁ„o o elemento ser· inserido. [%d]", cont+1);//+1 pq o usuario n„o entende posiÁ„o zero
+		printf("\nDigite 5 para escolher em que posiÁ„oo o elemento ser· inserido. [%d]", cont + 1); //+1 pq o usuario n„o entende posiùùo zero
 		printf("\nDigite 6 para verificar quantos elementos a lista possui");
 		printf("\n\nEscolha sua opÁ„o: ");
 		scanf("%d", &op);
-		switch(op)
+		switch (op)
 		{
-			case 1:
-				printf("\nDigite o elemento que ser· inserido no inÌcio: ");
-				scanf("%d", &valor);
-				inserirNoInicio(valor);
-				break;
-			case 2: 
-				printf("\n\t--Lista--\n");
-				mostraLista();
-				break;
-			case 3:
-				printf("\n\t--Lista--\n");
-				mostraListaInversa();
-				break;
-			case 4:
-				printf("\nDigite o elemento que ser· inserido no final: ");
-				scanf("%d", &valor);
-				inserirNoFim(valor);
-				break;
-			case 5: 
-				printf("Em qual posiÁ„o ser· inserido o elemento?: ");
-				scanf("%d", &valor);
-				if(valor > cont){
-					printf("\nO valor do elemento È maior do que a lista! ");
-					getch();
-					system("cls");
-				}
-				else{
-					inserirElementoPosicao(valor);
-				}
-				
-				break;
-			case 6:
-				tamanhoLista();
-				break;
-			default:
-				printf("\nOpÁ„o invalida!") ;
-				break;
+		case 1:
+			printf("\nDigite o elemento que ser· inserido no inÌcio: ");
+			scanf("%d", &valor);
+			inserirNoInicio(valor);
+			break;
+		case 2:
+			printf("\n\t--Lista--\n");
+			mostraLista();
+			break;
+		case 3:
+			printf("\n\t--Lista--\n");
+			mostraListaInversa();
+			break;
+		case 4:
+			printf("\nDigite o elemento que ser· inserido no final: ");
+			scanf("%d", &valor);
+			inserirNoFim(valor);
+			break;
+		case 5:
+			printf("\nEm qual posiÁ„o ser· inserido o elemento?: ");
+			scanf("%d", &posicao);
+			inserirElementoPosicao(posicao);
+			break;
+		case 6:
+			tamanhoLista();
+			break;
+		case 0:
+			break;
+		default:
+			printf("\nOpÁ„o inv·lida!");
+			break;
 		}
-		
-	}while(op != 0);
+
+	} while (op != 0);
 	printf("\n-----------Programa encerrado-----------");
 }
